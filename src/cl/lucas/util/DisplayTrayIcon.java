c2b1840b25dfa27265d367ea8c15f2a20ce46de3
lucaslizama <lucaslizama3@hotmail.com>
2015-02-06 21:01:52 -0300

    Creados los metodos "InitComponents()" para inicializar y configurar los atributos del TrayIcon y "addListeners()" para añadir los escuchadores de eventos a cada elemento del menu popup del TrayIcon.
    Ambos metodos añadidos al constructor para mayor organizacion del codigo.
    
    Ademas cambie todas las variables locales por variables globales

--- src/cl/lucas/util/DisplayTrayIcon.java
+++ src/cl/lucas/util/DisplayTrayIcon.java
@@ -3,6 +3,7 @@ package cl.lucas.util;
 import cl.lucas.nota.Nota;
 import java.awt.AWTException;
 import java.awt.Image;
+import java.awt.MenuItem;
 import java.awt.PopupMenu;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
@@ -16,30 +17,55 @@ import javax.swing.ImageIcon;
 public class DisplayTrayIcon {
     
     private TrayIcon trayIcon;
-    
+    private PopupMenu popUp;
+    private SystemTray tray;
+    private MenuItem aboutItem;
+    private MenuItem exitItem;
+    private MenuItem changeFontItem;
+                 
     public DisplayTrayIcon() {
         showTrayIcon();
     }
     
     private void showTrayIcon() {
-        
         if(!SystemTray.isSupported()){
             System.out.println("System is not supported");
             System.exit(0);
         }else{
-            final PopupMenu popUp = new PopupMenu();
-            final SystemTray tray = SystemTray.getSystemTray();
-            trayIcon = new TrayIcon(createIcon("/cl/lucas/images/tray_icon.png"),"Tray Icon");
-            trayIcon.setToolTip("Notas Adhesivas");
-            trayIcon.setPopupMenu(popUp);
-            
+            initComponents();
+            addListeners();
             
             try{
                 tray.add(trayIcon);
-            }catch(AWTException e){
-                
+            }catch(AWTException e){  
             }
         }
+    }
+    
+    private void initComponents() {
+        //Inicializamos los objetos SystemTray y PopupMenu
+        tray = SystemTray.getSystemTray();
+        popUp = new PopupMenu();
+        
+        //Inicializamos los elementos del PopUp Menu
+        aboutItem = new MenuItem("About");
+        exitItem = new MenuItem("Exit");
+        changeFontItem = new MenuItem("Change Font");
+        
+        //Añadimos los elemntos del menu al PopupMenu
+        popUp.add(changeFontItem);
+        popUp.add(aboutItem);
+        popUp.addSeparator();
+        popUp.add(exitItem);
+        
+        //Inicializamos el TrayIcon
+        trayIcon = new TrayIcon(createIcon("/cl/lucas/images/tray_icon.png"),"Tray Icon");
+        trayIcon.setToolTip("Notas Adhesivas");
+        trayIcon.setPopupMenu(popUp);
+    }
+    
+    private void addListeners() {
+        
     }
     
     private Image createIcon(String path){
