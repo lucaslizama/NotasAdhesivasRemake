1f1cc5a40a0c0aa41d3c339070b9cf83dc8a6f11
lucaslizama <lucaslizama3@hotmail.com>
2015-02-07 04:36:12 -0300

    Añadida clase Controller para gestion e interaccion entre la interfaz grafica de las notas y el modelo del programa.

---
+++ src/cl/lucas/App/Controller.java
@@ -0,0 +1,34 @@
+package cl.lucas.App;
+
+import java.util.ArrayList;
+import java.util.List;
+import cl.lucas.nota.Nota;
+
+/**
+ * Clase encargada de gestionar y almacenar cada una de las
+ * notas adhesivas creadas.
+ * @author Lucas Lizama
+ */
+public class Controller {
+    
+    private static final List<Nota> notas = new ArrayList<>();
+
+    public Controller() {
+        //Añadir metodo que compruebe si existen notas guardadas,
+        //Si existen debe cargarlas desde el archvivo.
+        //En caso contrario debera crear una nota, guardarla en la lista
+        //y luego crear un archivo para almacenar notas.
+    }
+
+    
+    
+    
+    //<editor-fold defaultstate="collapsed" desc="Accesadores y Mutadores">
+    /**
+     * @return Retorna la lista con las notas almacenadas.
+     */
+    public static List<Nota> getNotas() {
+        return notas;
+    }
+    //</editor-fold>
+}
